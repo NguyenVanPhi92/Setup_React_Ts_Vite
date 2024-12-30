93d2abc57a750ab345eb59ab7f32fe6993fe744e
KJ1002 <nguyenvanphidmx123455@gmail.com>
2024-12-30 13:19:32 +0700

    build structure project

---
+++ src/v2/@types/i18next.d.ts
@@ -0,0 +1,10 @@
+import 'i18next'
+import { defaultNS, resources } from 'src/i18n/i18n'
+// extend module 'i18next' and add new type
+declare module 'i18next' {
+  // thêm các type này vào module i18next
+  interface CustomTypeOptions {
+    defaultNS: typeof defaultNS
+    resources: (typeof resources)['vi']
+  }
+}
---
+++ src/v3/index.tsx
@@ -0,0 +1,5 @@
+const index = () => {
+  return <div>Coming soon....</div>
+}
+
+export default index
